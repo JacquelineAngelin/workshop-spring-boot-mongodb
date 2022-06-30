38316bc33a4c3bbf90512f6504e13439010c8512
Jacqueline Angelin <jacqueline.angelin@yahoo.com.br>
2022-06-30 08:57:48 -0300

    Connection with Mongodb repository and service

--- src/main/java/com/jacquelineAngelin/workshopmongo/WorkshopmongoApplication.java
+++ src/main/java/com/jacquelineAngelin/workshopmongo/WorkshopmongoApplication.java
@@ -1,4 +1,4 @@
-package com.workshopmongo;
+package com.jacquelineAngelin.workshopmongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/jacquelineAngelin/workshopmongo/domain/User.java
+++ src/main/java/com/jacquelineAngelin/workshopmongo/domain/User.java
@@ -1,11 +1,15 @@
 package com.workshopmongo.domain;
 
-import java.io.Serializable;
-import java.util.Objects;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.io.Serializable;
+
+@Document(collection = "user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
     private String id;
     private String name;
     private String email;
@@ -44,19 +48,27 @@ public class User implements Serializable {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof User)) return false;
-        User user = (User) o;
-        return getId().equals(user.getId());
-    }
-
-    @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((id == null)?0:id.hashCode());
-        //return Objects.hash(getId());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
         return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
     }
 }
